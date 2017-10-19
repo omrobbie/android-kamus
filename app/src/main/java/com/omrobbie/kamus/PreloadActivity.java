c6b42a80761bd8785f961d1025fe98ad14d17873
omrobbie <putu.roby@yahoo.com>
2017-10-19 15:30:16 +0800

    setup preload data layout

---
+++ app/src/main/java/com/omrobbie/kamus/PreloadActivity.java
@@ -0,0 +1,13 @@
+package com.omrobbie.kamus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preload);
+    }
+}
