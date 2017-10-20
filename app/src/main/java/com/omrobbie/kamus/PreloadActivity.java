21427f832c1696810aba563e057635a7c8c5dc24
omrobbie <putu.roby@yahoo.com>
2017-10-20 09:34:41 +0800

    fill progress bar using countdowntimer

--- app/src/main/java/com/omrobbie/kamus/PreloadActivity.java
+++ app/src/main/java/com/omrobbie/kamus/PreloadActivity.java
@@ -1,13 +1,44 @@
 package com.omrobbie.kamus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PreloadActivity extends AppCompatActivity {
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progress_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preload);
+
+        ButterKnife.bind(this);
+
+        loadDummyProcess();
+    }
+
+    private void loadDummyProcess() {
+        final int countDown = 1000;
+        progress_bar.setMax(countDown);
+        CountDownTimer countDownTimer = new CountDownTimer(countDown, (countDown / 100)) {
+            @Override
+            public void onTick(long l) {
+                progress_bar.setProgress((int) (countDown - l));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 }
